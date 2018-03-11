778fe8ed8f5088c865145f80cae04096c0f20b3d
Giannis Lam <john-lamboff@outlook.com>
2018-03-11 21:22:49 +0200

    posting list

---
+++ posting_list_node.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "posting_list_node.h"
+
+void set_num(int input) {
+
+}
+
+
+void set_next_ptr(int ptr) {
+
+}
+
+int get_id() {
+
+}
+    
+int get_num() {
+
+}
+    
+int get_next_ptr() {
+    
+}
